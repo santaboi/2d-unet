39a5232e31a48fb0d74fb801dcc44f4203ef3a32
Yang <egg20001225@gmail.com>
2021-12-02 19:42:17 +0800

    data aug

--- cfg.py
+++ cfg.py
@@ -1,2 +1,6 @@
-non_flair_data = './data/PSPF_voc_data/non_PR/Flair/JPEGImages/'
-non_flair_target = './data/PSPF_voc_data/non_PR/Flair/SegmentationClassPNG/'
+#only use T1C data (non PR)
+
+#whole brain
+non_T1C_data = "./data/PSPF_voc_data/non_PR/T1c/JPEGImages/"  
+#mask (ground truth)
+non_T1C_target = "./data/PSPF_voc_data/non_PR/T1c/SegmentationClassPNG/" 
