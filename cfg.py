46bbfef419ff63cf5d9088f1ea6da93323e56803
Yang <egg20001225@gmail.com>
2021-12-02 20:46:59 +0800

    total data load complete

--- cfg.py
+++ cfg.py
@@ -1,6 +1,26 @@
 #only use T1C data (non PR)
 
 #whole brain
-non_T1C_data = "./data/PSPF_voc_data/non_PR/T1c/JPEGImages/"  
+non_T1C_data = "./data/PSPF_voc_data/non_PR/T1c/JPEGImages"  
+PR_T1C_data = "./data/PSPF_voc_data/PR/T1c/JPEGImages"
+
+non_T1_data = "./data/PSPF_voc_data/non_PR/T1/JPEGImages"
+PR_T1_data = "./data/PSPF_voc_data/PR/T1/JPEGImages"
+
+non_Flair_data = "./data/PSPF_voc_data/non_PR/Flair/JPEGImages"
+PR_Flair_data = "./data/PSPF_voc_data/PR/Flair/JPEGImages"
+
+non_T2_data = "./data/PSPF_voc_data/non_PR/T2/JPEGImages"
+PR_T2_data = "./data/PSPF_voc_data/PR/T2/JPEGImages"
 #mask (ground truth)
-non_T1C_target = "./data/PSPF_voc_data/non_PR/T1c/SegmentationClassPNG/" 
+non_T1C_target = "./data/PSPF_voc_data/non_PR/T1c/SegmentationClassPNG" 
+PR_T1C_target = "./data/PSPF_voc_data/PR/T1c/SegmentationClassPNG"
+
+non_T1_target = "./data/PSPF_voc_data/non_PR/T1/SegmentationClassPNG" 
+PR_T1_target = "./data/PSPF_voc_data/PR/T1/SegmentationClassPNG"
+
+non_Flair_target = "./data/PSPF_voc_data/non_PR/Flair/SegmentationClassPNG" 
+PR_Flair_target = "./data/PSPF_voc_data/PR/Flair/SegmentationClassPNG"
+
+non_T2_target = "./data/PSPF_voc_data/non_PR/T2/SegmentationClassPNG" 
+PR_T2_target = "./data/PSPF_voc_data/PR/T2/SegmentationClassPNG"
